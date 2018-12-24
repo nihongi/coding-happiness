6421e95f90acb6557e76ecca323441322d787505
Nobuyoshi Nihongi <nihongi@localhost.localdomain>
2018-12-24 04:50:16 -0500

    デフォルトの日付を現在の日付にした。

--- zeller/zeller.sh
+++ zeller/zeller.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 WEEK_DAY=("Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
-YEAR_DEFAULT=1970
-MONTH_DEFAULT=1
-DATE_DEFAULT=1
-: ${Y=${1:-$YEAR_DEFAULT}}
-: ${M=${2:-$MONTH_DEFAULT}}
-: ${D=${3:-$DATE_DEFAULT}}
+NOW=($(date "+%Y %m %d"))
+: ${Y=${1:-${NOW[0]}}}
+: ${M=${2:-${NOW[1]}}}
+: ${D=${3:-${NOW[2]}}}
 echo $Y"/"$M"/"$D
 echo ${WEEK_DAY[$(( ( $Y + $Y / 4 - $Y / 100 + $Y / 400 + ( 13 * $M + 8 ) / 5 + $D ) % 7 ))]}
