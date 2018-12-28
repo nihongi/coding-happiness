149931d85198d88c1ae959e46e680f6e2fdfb109
Nobuyoshi Nihongi <nihongi@gmail.com>
2018-12-29 01:25:41 +0900

    ドキュメント作成、大文字小文字修正

--- fizzbuzz/fizzbuzz.sh
+++ fizzbuzz/fizzbuzz.sh
@@ -2,12 +2,12 @@
 for i in {1..100}
 do
   if [ $(( ${i} % 3 )) -eq 0 ]; then
-    s="fizz"
+    s="Fizz"
   else
     unset s
   fi
   if [ $(( ${i} % 5 )) -eq 0 ]; then
-    s=${s}"buzz"
+    s=${s}"Buzz"
   fi
   echo ${s-${i}}
 done
