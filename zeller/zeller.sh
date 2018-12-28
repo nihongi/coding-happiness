056538c90875cdb18832fb7cf7e3bd08da43d850
NIHONGI <nihongi@node00.test>
2018-12-28 05:15:04 +0000

    fizzbuzz作成、zellerのデフォルト値の取り方を修正

--- zeller/zeller.sh
+++ zeller/zeller.sh
@@ -2,8 +2,8 @@
 WEEK_DAY=("Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
 DAY=($(echo $1 | sed -e 's/\// /g'))
 TODAY=($(date "+%Y %m %d"))
-: ${Y=${DAY[0]:-${TODAY[0]}}}
-: ${M=${DAY[1]:-${TODAY[1]}}}
-: ${D=${DAY[2]:-${TODAY[2]}}}
+Y=${DAY[0]:-${TODAY[0]}}
+M=${DAY[1]:-${TODAY[1]}}
+D=${DAY[2]:-${TODAY[2]}}
 echo $Y"/"$M"/"$D
 echo ${WEEK_DAY[$(( ( $Y + $Y / 4 - $Y / 100 + $Y / 400 + ( 13 * $M + 8 ) / 5 + $D ) % 7 ))]}
