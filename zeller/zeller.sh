6f022744993004a66eef547ee31ae099bd472e18
NIHONGI <nihongi@node00.test>
2018-12-28 05:20:34 +0000

    zellerの変数参照の修正

--- zeller/zeller.sh
+++ zeller/zeller.sh
@@ -5,5 +5,5 @@ TODAY=($(date "+%Y %m %d"))
 Y=${DAY[0]:-${TODAY[0]}}
 M=${DAY[1]:-${TODAY[1]}}
 D=${DAY[2]:-${TODAY[2]}}
-echo $Y"/"$M"/"$D
-echo ${WEEK_DAY[$(( ( $Y + $Y / 4 - $Y / 100 + $Y / 400 + ( 13 * $M + 8 ) / 5 + $D ) % 7 ))]}
+echo "${Y}/${M}/${D}"
+echo ${WEEK_DAY[$(( ( ${Y} + ${Y} / 4 - ${Y} / 100 + ${Y} / 400 + ( 13 * ${M} + 8 ) / 5 + ${D} ) % 7 ))]}
