076838005f20f3fef5a435d6c2e7f75d679a86d6
NIHONGI <nihongi@node00.test>
2018-12-28 05:21:31 +0000

    変数参照の修正2

--- zeller/zeller.sh
+++ zeller/zeller.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 WEEK_DAY=("Sunday" "Monday" "Tuesday" "Wednesday" "Thursday" "Friday" "Saturday")
-DAY=($(echo $1 | sed -e 's/\// /g'))
+DAY=($(echo ${1} | sed -e 's/\// /g'))
 TODAY=($(date "+%Y %m %d"))
 Y=${DAY[0]:-${TODAY[0]}}
 M=${DAY[1]:-${TODAY[1]}}
