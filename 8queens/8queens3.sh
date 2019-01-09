d095e6ed53ba5b4a4a15f3544d6f03aee43704f3
Nobuyoshi Nihongi <nihongi@localhost.localdomain>
2019-01-09 05:38:58 -0500

    整理

--- 8queens/8queens3.sh
+++ 8queens/8queens3.sh
@@ -16,7 +16,7 @@ put() {
     wait ${children}
     for i in ${children}
     do
-      if [ -e /tmp/nq.${i} ]; then
+      if [ -f /tmp/nq.${i} ]; then
         cat /tmp/nq.${i}
         rm /tmp/nq.${i}
       fi
@@ -26,7 +26,6 @@ put() {
     # もしi行目にクイーンを置いたら(${2}-1)列目までに衝突があるかを調べる
     # 衝突がなければクイーンを置いて次の列に行く
     children=""
-    tmpfiles=""
     for i in {0..7}
     do
       for j in `seq 0 $(( ${2} - 1 ))`  # j列目
@@ -51,21 +50,21 @@ put() {
       # 衝突なし
       tmp=${1:0:$(( ${2} + 8 * ${i} ))}"1"${1:$(( ${2} + 8 * ${i} + 1 ))}
       if [ ${2} = 7 ]; then
-        pid=${BASHPID}
-        echo ${tmp} >> /tmp/nq.${pid}
-        echo "debug2 ${pid}" >&2
+        echo ${tmp} >> /tmp/nq.${BASHPID}
       else
         (put ${tmp} $(( ${2} + 1 )))&
         children="${children} $!"
       fi
     done
     wait ${children}
-    pid=${BASHPID}
     for i in ${children}
     do
-      if [ -e /tmp/nq.${i} ]; then
-        cat /tmp/nq.${i} >> /tmp/nq.${pid}
-        echo "debug3 ${pid}" >&2
+      if [ -f /tmp/nq.${i} ]; then
+        OUT=$(</tmp/nq.${i})
+        for o in ${OUT}
+        do
+          echo ${o} >> /tmp/nq.${BASHPID}
+        done
         rm /tmp/nq.${i}
       fi
     done
