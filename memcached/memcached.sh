f94868c3094ef47a17537bcf36a72964dc5a38c8
Nobuyoshi Nihongi <nihongi@localhost.localdomain>
2019-01-11 19:50:04 -0500

    改善

--- memcached/memcached.sh
+++ memcached/memcached.sh
@@ -2,18 +2,23 @@ MC_HOST="localhost"
 MC_PORT="11211"
 
 mc_open() {
-  coproc mc { telnet ${MC_HOST} ${MC_PORT}; } 2>/dev/null
-  # telnet接続時の出力を3行分読み飛ばす
-  read <&"${mc[0]}"    # Trying ::1...
-  read <&"${mc[0]}"    # Connected to localhost.
-  read <&"${mc[0]}"    # Escape character is '^]'.
+  if [ -z ${mc} ]; then
+    coproc mc { telnet ${MC_HOST} ${MC_PORT}; } 2>/dev/null
+    # telnet接続時の出力を読み飛ばす (最後が'^]'.なら終わり)
+    while read -t 3 r <&"${mc[0]}"
+    do
+      if [ "${r:$(( ${#r[@]} - 7 )):5}" = "'^]'." ]; then
+        break
+      fi
+    done
+  fi
 }
 
 mc_set() {
   local r
   echo "set ${1} 0 ${2} ${#3}" >&"${mc[1]}"
   echo "${3}" >&"${mc[1]}"
-  read r <&"${mc[0]}"
+  read -t 3 r <&"${mc[0]}"
   if [ "${r}" = "STORED" ]; then
     return 0
   else
@@ -27,7 +32,7 @@ mc_get() {
   echo "get ${1}" >&"${mc[1]}"
   for i in {0..2}      # 値があれば3行目、無ければ1行目が"END"
   do
-    read r <&"${mc[0]}"
+    read -t 3 r <&"${mc[0]}"
     if [ "${r}" = "END" ]; then
       echo -n ${v}
       break
@@ -39,7 +44,10 @@ mc_get() {
 }
 
 mc_close() {
-  echo "quit" >&"${mc[1]}"
+  if [ ! -z ${mc} ]; then
+    echo "quit" >&"${mc[1]}"
+    unset mc
+  fi
 }
 
 
--- memcached/sample.sh
+++ memcached/sample.sh
@@ -4,6 +4,11 @@
 # 接続開始
 mc_open
 
+#if [ ! -z mc ]; then
+#  echo "Already open." >&2
+#fi
+#echo ${mc[@]} >&2
+
 # 値のsetは キー 有効期間 値 を指定
 mc_set mykey1 10 myvalue1
 
@@ -22,3 +27,4 @@ fi
 # 接続終了
 mc_close
 
+#echo ${mc[@]} >&2
