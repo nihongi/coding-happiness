55e2354c89499dff9153cb6df83f7c1603b77009
Nobuyoshi Nihongi <nihongi@localhost.localdomain>
2019-01-12 09:51:37 -0500

    tarai対応

--- memcached/memcached.sh
+++ memcached/memcached.sh
@@ -2,23 +2,24 @@ MC_HOST="localhost"
 MC_PORT="11211"
 
 mc_open() {
-  if [ -z ${mc} ]; then
-    coproc mc { telnet ${MC_HOST} ${MC_PORT}; } 2>/dev/null
-    # telnet接続時の出力を読み飛ばす (最後が'^]'.なら終わり)
-    while read -t 3 r <&"${mc[0]}"
-    do
-      if [ "${r:$(( ${#r[@]} - 7 )):5}" = "'^]'." ]; then
-        break
-      fi
-    done
-  fi
+  mypid=${BASHPID}
+  eval "coproc mc${mypid} { telnet ${MC_HOST} ${MC_PORT}; } 2>/dev/null"
+  # telnet接続時の出力を読み飛ばす (最後が'^]'.なら終わり)
+  eval "mc_out=\${mc${mypid}[0]}"
+  eval "mc_in=\${mc${mypid}[1]}"
+  while read -t 3 r <&"${mc_out}"
+  do
+    if [ "${r:$(( ${#r[@]} - 7 )):5}" = "'^]'." ]; then
+      break
+    fi
+  done
 }
 
 mc_set() {
   local r
-  echo "set ${1} 0 ${2} ${#3}" >&"${mc[1]}"
-  echo "${3}" >&"${mc[1]}"
-  read -t 3 r <&"${mc[0]}"
+  echo "set ${1} 0 ${2} ${#3}" >&"${mc_in}"
+  echo "${3}" >&"${mc_in}"
+  read -t 3 r <&"${mc_out}"
   if [ "${r}" = "STORED" ]; then
     return 0
   else
@@ -29,10 +30,10 @@ mc_set() {
 
 mc_get() {
   local i r v
-  echo "get ${1}" >&"${mc[1]}"
+  echo "get ${1}" >&"${mc_in}"
   for i in {0..2}      # 値があれば3行目、無ければ1行目が"END"
   do
-    read -t 3 r <&"${mc[0]}"
+    read -t 3 r <&"${mc_out}"
     if [ "${r}" = "END" ]; then
       echo -n ${v}
       break
@@ -44,10 +45,7 @@ mc_get() {
 }
 
 mc_close() {
-  if [ ! -z ${mc} ]; then
-    echo "quit" >&"${mc[1]}"
-    unset mc
-  fi
+  echo "quit" >&"${mc_in}"
 }
 
 
