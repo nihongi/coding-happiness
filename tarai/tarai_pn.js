781251ddb18c885a87158b57bf808e6e30f241d2
NIHONGI <nihongi@node00.test>
2019-01-17 05:19:46 +0000

    デバッグ

--- tarai/tarai_pn.js
+++ tarai/tarai_pn.js
@@ -2,27 +2,29 @@
 // var formula=process.argv[2];
 
 var formula = ['t','t', '1', '1', '0', 't', '0', '0', '2', 't', '-1', '2', '1'];
+//var formula = ['t', '1', '1', '0'];
 console.error(formula);
 
 var p;       // ポインタ
 var start;   // 計算する部分の開始位置
 var end;     // 計算する部分の終了位置
-var tmp_end; // 計算する部分の仮の終了位置
+//var tmp_end; // 計算する部分の仮の終了位置
 var result;  // 計算する部分の計算結果、未定の場合は-10000
 
 //while(true) {  // 式の変形結果が数値になるまで繰り返す
 //  if(! isNaN(formula[0])) break;
   p = 0;
 //  result = -10000;
+  set_range:
   while(true) {  // 計算可能な式の開始位置と終了位置を決める
     if('t' == formula[p]) {
       if(isNaN(formula[p + 1])) {
         p += 1;
-        break;
+//        continue;
       }
       else if(isNaN(formula[p + 2])) {
         p += 2;
-        break;
+//        continue;
       }
       else {  // 「t, <数値>, <数値>, <式 or 数値>」を見つけた
         if(formula[p + 1] <= formula[p + 2]) {
@@ -30,21 +32,20 @@ var result;  // 計算する部分の計算結果、未定の場合は-10000
           result = formula[p + 2];
         }
         start = p;
-        tmp_end = p + 3;
-        p = tmp_end;
+        end = p + 3;
+        p = end;
         while(true) {  // 式の終了位置を探す
           if(! isNaN(formula[p])) {
-            if(p == tmp_end) {
-              end = p;
-              break;
+            if(p == end) {
+              break set_range;
             }
           }
           else {
             if('t' == formula[p]) {
-              tmp_end += 3;
+              end += 3;
             }
             else { // 's' == formula[p]
-              tmp_end += 1;
+              end += 1;
             }
           }
           p += 1;
@@ -54,12 +55,12 @@ var result;  // 計算する部分の計算結果、未定の場合は-10000
     else if('s' == formula[p]) {
       if(isNaN(formula[p + 1])) {
         p += 1;
-        break;
       }
       else {  // 「s, <数値>」を見つけた
         start = p;
         end = p + 1;
         result = formula[p + 1] - 1;
+        break;
       }
     }
     else {
@@ -67,8 +68,7 @@ var result;  // 計算する部分の計算結果、未定の場合は-10000
     }
   }
 
-console.error(start);
-console.error(end);
+console.error("start = %d, end = %d", start, end);
   // 計算可能な式の開始位置と終了位置が決まった
 
   // resultが計算済みの場合 (式を縮める方向)
